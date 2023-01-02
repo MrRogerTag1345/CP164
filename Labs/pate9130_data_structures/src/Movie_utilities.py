28cd8f87e6bf9564ef3121301d21c6f79fd0ca1b
Chetas Patel <94209839+MrRogerTag1345@users.noreply.github.com>
2023-01-02 14:45:18 -0500

    Add files via upload

---
+++ Labs/pate9130_data_structures/src/Movie_utilities.py
@@ -0,0 +1,286 @@
+"""
+-------------------------------------------------------
+Movie class utility functions.
+-------------------------------------------------------
+Author:  David Brown
+ID:      999999999
+Email:   dbrown@wlu.ca
+Section: CP164 B
+__updated__ = "2021-09-23"
+-------------------------------------------------------
+"""
+
+from Movie import Movie
+
+
+def get_movie():
+    """
+    -------------------------------------------------------
+    Creates a Movie object by requesting data from a user.
+    Use: movie = get_movie()
+    -------------------------------------------------------
+    Returns:
+        movie - a Movie object based upon the user input (Movie).
+    -------------------------------------------------------
+    """
+    
+    title = input("Title: ")
+    year = int(input("Year of release: "))
+    dir = input("Director: ")
+    rate = float(input("Rating: "))
+    genre = read_genres()
+    
+    movie = Movie(title, year, dir, rate, genre)
+
+    return movie
+
+
+def read_movie(line):
+    """
+    -------------------------------------------------------
+    Creates and returns a Movie object from a line of formatted string data.
+    Use: movie = read_movie(line)
+    -------------------------------------------------------
+    Parameters:
+        line - a vertical bar-delimited line of movie data in the format
+          title|year|director|rating|genre codes (str)
+    Returns:
+        movie - a Movie object based upon the data from line (Movie)
+    -------------------------------------------------------
+    """
+    # Declaring Variables.
+    temp = []
+    test = []
+    genres = []
+    
+    # Split into different list.
+    temp = line.split("|")
+    test = temp[4].split(",")
+    
+    # Loops convert test into int list.
+    for i in range(len(test)):
+        genres.append(int(test[i]))
+
+    # Calls Movie Object.
+    movie = Movie(temp[0], int(temp[1]), temp[2], float(temp[3]), genres)
+    
+    # Return Statement.
+    return movie
+
+
+def read_movies(fv):
+    """
+    -------------------------------------------------------
+    Reads a file of string data into a list of Movie objects.
+    Use: movies = read_movies(fv)
+    -------------------------------------------------------
+    Parameters:
+        fv - an already open file of movie data (file)
+    Returns:
+        movies - a list of Movie objects (list of Movie)
+    -------------------------------------------------------
+    """
+
+    fv.seek(0)
+    movies = []
+    line = fv.readline()
+    while line != "":
+        movie = read_movie(line)
+        movies.append(movie)
+        line = fv.readline()
+        
+    return movies
+
+
+def read_genres():
+    """
+    -------------------------------------------------------
+    Asks a user to select genres from a list of genres and returns
+    an integer list of the genres chosen.
+    Use: genres = read_genres()
+    -------------------------------------------------------
+    Returns:
+        genres - sorted numeric list of movie genres (list of int)
+    -------------------------------------------------------
+    """
+    
+    genres = []
+
+    string = Movie.genres_menu()
+    print("{}".format(string))
+    
+    temp = input("Enter a genre number (ENTER to quit): ")
+    while genres == [] or temp != "":
+        if temp.isdigit() == True:
+            if int(temp) in genres:
+                print("Error: genre already chosen")
+            elif int(temp) > len(Movie.GENRES):
+                print("Error: input must be < 10")
+            else:
+                genres.append(int(temp))
+        else:
+            print("Error: not a positive number.")
+        
+        temp = input("Enter a genre number (ENTER to quit): ")
+            
+    genres.sort()
+    return genres
+
+
+def write_movies(fv, movies):
+    """
+    -------------------------------------------------------
+    Writes the contents of movies to fv. Overwrites or
+    creates a new file of Movie objects converted to strings.
+    Use: write_movies(fv, movies)
+    -------------------------------------------------------
+    Parameters:
+        fv - an already open file of movie data (file)
+        movies - a list of Movie objects (list of Movie)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return
+
+
+def get_by_year(movies, year):
+    """
+    -------------------------------------------------------
+    Creates a list of Movies from a particular year.
+    The original list of movies must be unchanged.
+    Use: ymovies = get_by_year(movies, year)
+    -------------------------------------------------------
+    Parameters:
+        movies - a list of Movie objects (list of Movie)
+        year - the Movie year to select (int)
+    Returns:
+        ymovies - Movie objects whose year attribute is
+            year (list of Movie)
+    -------------------------------------------------------
+    """
+
+    ymovie = []
+    
+    for i in movies:
+        if i.year == year:
+            ymovie.append(i)
+
+    return ymovie
+
+
+def get_by_rating(movies, rating):
+    """
+    -------------------------------------------------------
+    Creates a list of Movies whose ratings are equal to or higher
+    than rating.
+    The original list of movies must be unchanged.
+    Use: rmovies = get_by_rating(movies, rating)
+    -------------------------------------------------------
+    Parameters:
+        movies - a list of Movie objects (list of Movie)
+        rating - the minimum Movie rating to select (float)
+    Returns:
+        rmovies - Movie objects whose rating attribute is
+            greater than or equal to rating (list of Movie)
+    -------------------------------------------------------
+    """
+
+    rmovie = []
+    
+    for i in movies:
+        if i.rating >= rating:
+            rmovie.append(i)
+
+    return rmovie
+
+
+def get_by_genre(movies, genre):
+    """
+    -------------------------------------------------------
+    Creates a list of Movies whose list of genres include genre.
+    The original list of movies must be unchanged.
+    Use: gmovies = get_by_genre(movies, genre)
+    -------------------------------------------------------
+    Parameters:
+        movies - a list of Movie objects (list of Movie)
+        genre - the genre code to look for (int)
+    Returns:
+        gmovies - Movie objects whose genre list includes
+            genre (list of Movie)
+    -------------------------------------------------------
+    """
+
+    gmovie = []
+    
+    for i in movies:
+        for j in i.genres:
+            if j == genre:
+                gmovie.append(i)
+
+    return gmovie
+
+
+def get_by_genres(movies, genres):
+    """
+    -------------------------------------------------------
+    Creates a list of Movies whose list of genres include all the genre
+    codes in genres.
+    The original list of movies must be unchanged.
+    Use: m = get_by_genres(movies, genres)
+    -------------------------------------------------------
+    Parameters:
+        movies - a list of Movie objects (list of Movie)
+        genres - the genre codes to look for (list of int)
+    Returns:
+        gmovies - Movie objects whose genre list includes
+            all the genres in genres (list of Movie)
+    -------------------------------------------------------
+    """
+
+    gmovies = []
+    count = 0
+    i = 0
+    
+    while i < len(movies):
+        temp = movies[i]
+        
+        for j in genres:
+            if j in temp.genres:
+                count += 1
+                
+        if count == len(genres) and count == len(temp.genres):
+            gmovies.append(temp)
+        
+        i += 1
+        count = 0
+    
+    return gmovies
+
+
+def genre_counts(movies):
+    """
+    -------------------------------------------------------
+    Counts the number of movies in each genre given in Movie.GENRES.
+    The original list of movies must be unchanged.
+    Use: counts = genre_counts(movies)
+    -------------------------------------------------------
+    Parameters:
+        movies - a list of Movie objects (list of Movie)
+    Returns:
+        counts - the number of Movies in each genre in Movie.GENRES.
+            The index of each number in counts is the index of
+            the matching genre in Movie.GENRES. (list of int)
+    -------------------------------------------------------
+    """
+
+    counts = [0] * len(Movie.GENRES)
+    
+    for i in movies:
+        for j in i.genres:
+            counts[j] += 1
+            
+    return counts
---
+++ Labs/pate9130_data_structures/src/Queue_linked.py
@@ -0,0 +1,303 @@
+"""
+-------------------------------------------------------
+Linked version of the Queue ADT.
+-------------------------------------------------------
+Author:  David Brown
+ID:      999999999
+Email:   dbrown@wlu.ca
+Term:    Spring 2021
+__updated__ = "2021-11-04"
+-------------------------------------------------------
+"""
+from copy import deepcopy
+
+
+class _Queue_Node:
+
+    def __init__(self, value, next_):
+        """
+        -------------------------------------------------------
+        Initializes a queue node that contains a copy of value
+        and a link to the next node in the queue.
+        Use: node = _Queue_Node(value, _next)
+        -------------------------------------------------------
+        Parameters:
+            value - value for node (?)
+            next_ - another Queue node (_Queue_Node)
+        Returns:
+            a new _Queue_Node object (_Queue_Node)
+        -------------------------------------------------------
+        """
+        self._value = deepcopy(value)
+        self._next = next_
+
+
+class Queue:
+
+    def __init__(self):
+        """
+        -------------------------------------------------------
+        Initializes an empty queue. Values are stored in a
+        linked structure.
+        Use: queue = Queue()
+        -------------------------------------------------------
+        Returns:
+            a new Queue object (Queue)
+        -------------------------------------------------------
+        """
+        self._front = None
+        self._rear = None
+        self._count = 0
+
+    def is_empty(self):
+        """
+        -------------------------------------------------------
+        Determines if the queue is empty.
+        Use: b = queue.is_empty()
+        -------------------------------------------------------
+        Returns:
+            True if queue is empty, False otherwise.
+        -------------------------------------------------------
+        """
+        # your code here
+        return self._count == 0
+
+    def is_full(self):
+        """
+        -------------------------------------------------------
+        Determines if the queue is full.
+        Use: b = queue.is_full()
+        -------------------------------------------------------
+        Returns:
+            True if queue is full, False otherwise.
+        -------------------------------------------------------
+        """
+        # your code here
+        return False
+
+    def __len__(self):
+        """
+        -------------------------------------------------------
+        Returns the length of the queue.
+        Use: n = len(queue)
+        -------------------------------------------------------
+        Returns:
+            the number of values in queue.
+        -------------------------------------------------------
+        """
+        # your code here
+        return self._count
+
+    def insert(self, value):
+        """
+        -------------------------------------------------------
+        Inserts a copy of value into the queue.
+        Use: queue.insert(value)
+        -------------------------------------------------------
+        Parameters:
+            value - a data element (?)
+        Returns:
+            a copy of value is added to the rear of queue.
+        -------------------------------------------------------
+        """
+        node = _Queue_Node(value, None)
+
+        if self._front is None:
+            self._front = node
+        else:
+            self._rear._next = node
+
+        self._rear = node
+        self._count += 1
+        return
+
+    def remove(self):
+        """
+        -------------------------------------------------------
+        Removes and returns value from the queue.
+        Use: value = queue.remove()
+        -------------------------------------------------------
+        Returns:
+            value - the value at the front of the queue - the value is
+            removed from queue (?)
+        -------------------------------------------------------        
+        """
+        assert self._front is not None, "Cannot remove from an empty queue"
+
+        value = self._front
+        self._front = value._next
+        self._next = None
+        self._count -= 1
+        return value._value
+
+    def peek(self):
+        """
+        -------------------------------------------------------
+        Peeks at the front of queue.
+        Use: value = queue.peek()
+        -------------------------------------------------------
+        Returns:
+            value - a copy of the value at the front of queue -
+            the value is not removed from queue (?)
+        -------------------------------------------------------
+        """
+        assert self._front is not None, "Cannot peek at an empty queue"
+
+        # your code here
+        return deepcopy(self._front._value)
+
+    def _move_front(self, source):
+        """
+        -------------------------------------------------------
+        Moves the front node from the source queue to the rear of the target queue.
+        The target queue contains the old front node of the source queue.
+        The source queue front is updated.
+        Use: target._move_front(source)
+        -------------------------------------------------------
+        Parameters:
+            source - a linked queue (Queue)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        assert source._front is not None, "Cannot move the front of an empty queue"
+
+        # your code here
+        return
+
+    def _append_queue(self, source):
+        """
+        -------------------------------------------------------
+        Appends the entire source queue to the rear of the target queue.
+        The source queue becomes empty.
+        Use: target._append_queue(source)
+        -------------------------------------------------------
+        Parameters:
+            source - an linked-based queue (Queue)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        assert source._front is not None, "Cannot append an empty queue"
+
+        # your code here
+        return
+
+    def combine(self, source1, source2):
+        """
+        -------------------------------------------------------
+        Combines two source queues into the current target queue. 
+        When finished, the contents of source1 and source2 are interlaced 
+        into target and source1 and source2 are empty.
+        (iterative algorithm)
+        Use: target.combine(source1, source2)
+        -------------------------------------------------------
+        Parameters:
+            source1 - an linked queue (Queue)
+            source2 - an linked queue (Queue)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        left = True
+        var1 = source1._front
+        var2 = source2._front
+        while var1 is not None or var2 is not None:
+            if left:
+                if var1 is not None:
+                    new_node = _Queue_Node(var1._value, None)
+                    var1 = var1._next
+                else:
+                    new_node = _Queue_Node(var2._value, None)
+                    var2 = var2._next
+                
+            else:
+                if var2 is not None:
+                    new_node = _Queue_Node(var2._value, None)
+                    var2 = var2._next
+                else:
+                    new_node = _Queue_Node(var1._value, None)
+                    var1 = var1._next
+            
+            if self._front is None:          
+                self._front = new_node
+                self._rear = new_node
+            else:
+                self._rear._next = new_node
+                self._rear = new_node
+            
+            self._count += 1
+                    
+            left = not left
+            
+        source1._count = 0
+        source1._front = None
+        source1._rear = None
+        source2._count = 0
+        source2._front = None
+        source2._rear = None
+        return
+
+    def split_alt(self):
+        """
+        -------------------------------------------------------
+        Splits the source queue into separate target queues with values 
+        alternating into the targets. At finish source queue is empty.
+        (iterative algorithm)
+        Use: target1, target2 = source.split()
+        -------------------------------------------------------
+        Returns:
+            target1 - contains alternating values from source (Queue)
+            target2 - contains other alternating values from source (Queue)
+        -------------------------------------------------------
+        """
+        return
+
+    def is_identical(self, target):
+        """
+        -------------------------------------------------------
+        Determines whether two queues are identical.
+        Values of self and target are compared and if all contents 
+        are identical and in the same order, returns True, otherwise 
+        returns False. Queues are unchanged.
+        (iterative algorithm)
+        Use: b = source.is_identical(target)
+        -------------------------------------------------------
+        Parameters:
+            target - a queue (Queue)
+        Returns:
+            identical - True if self and target are identical, False 
+                otherwise. (boolean)
+        -------------------------------------------------------
+        """
+        # your code here
+        if self._count != target._count:
+            identical = False
+        else:
+            s_node = self._front
+            t_node = target._front
+
+            while s_node is not None and s_node._value == t_node._value:
+                s_node = s_node._next
+                t_node = t_node._next
+
+            identical = s_node is None
+        return identical
+
+    def __iter__(self):
+        """
+        USE FOR TESTING ONLY
+        -------------------------------------------------------
+        Generates a Python iterator. Iterates through the queue
+        from front to rear.
+        Use: for v in q:
+        -------------------------------------------------------
+        Returns:
+            value - the next value in the queue (?)
+        -------------------------------------------------------
+        """
+        current = self._front
+
+        while current is not None:
+            yield current._value
+            current = current._next
